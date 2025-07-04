dd9b4378fb39476c4eded72df6941609ff8f4785
DA M1 37 <dam137@DAs-MacBook-Air.local>
2025-07-04 14:54:21 +0200

    develop

--- file.js
+++ file.js
@@ -23,3 +23,15 @@ switch (new Date().getDay()) {
 }
 
 console.log(day)
+
+switch (new Date().getDay()) {
+  case 6:
+    text = "Today is Saturday";
+    break;
+  case 0:
+    text = "Today is Sunday";
+    break;
+  default:
+    text = "Looking forward to the Weekend";
+}
+console.log(text)
